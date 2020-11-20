64716df136b49bc4a2aeb34b6089ab49b539bc3b
kiranmarshall <kiran.marshall@gmail.com>
2020-11-20 14:15:56 +0000

    add sketchpad

--- Javascript Algorithms and Data Structures Certification/Javascript Algorithms and Data Structures Projects/Cash Register.js
+++ Javascript Algorithms and Data Structures Certification/Javascript Algorithms and Data Structures Projects/Cash Register.js
@@ -29,8 +29,8 @@
 // One-hundred Dollars	$100 (ONE HUNDRED)
 
 function checkCashRegister(price, cash, cid) {
-  var change;
-  return change;
+
+
 }
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
